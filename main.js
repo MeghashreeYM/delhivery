320c44692be8df5d81dc7481abdba5ecb7dce389
MeghashreeYM <63654370+MeghashreeYM@users.noreply.github.com>
2022-03-30 12:52:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function submit() {
+    console.log(document.getElementById("code").value);
+    console.log(document.getElementById("route").value);
+    console.log(document.getElementById("pl").value);
+}
